49d4c09d9106f6508b8a437af252c00c55d8a298
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-10-05 14:18:51 +0200

    button color

--- JS/script.js
+++ JS/script.js
@@ -22,24 +22,31 @@ function topFunction() {
 
 // CHANGE COLOR THEME
 
-var r=document.querySelector(':root')
+// var r=document.querySelector(':root')
 
-function myFunction_get() {
-  // Get the styles (properties and values) for the root
-  var rs = getComputedStyle(r);
-  // Alert the value of the --blue variable
-  alert("The value of --blue is: " + rs.getPropertyValue('--blue'));
-}
+// function myFunction_get() {
+//   // Get the styles (properties and values) for the root
+//   var rs = getComputedStyle(r);
+//   // Alert the value of the --blue variable
+//   alert("The value of --blue is: " + rs.getPropertyValue('--blue'));
+// }
 
-// Create a function for setting a variable value
-function myFunction_set() {
-  // Set the value of variable --blue to another value (in this case "lightblue")
-  r.style.setProperty('--blue', 'purple');
-}
+// // Create a function for setting a variable value
+// function myFunction_set() {
+//   // Set the value of variable --blue to another value (in this case "lightblue")
+//   r.style.setProperty('--blue', 'purple');
+// }
 
-document.getElementById('change').onclick = changeColor;   
+// document.getElementById('change').onclick = changeColor;   
 
-    function changeColor() {
-        document.body.style.r = "purple";
-        return false;
-    }   
+//     function changeColor() {
+//         document.body.style.r = "purple";
+//         return false;
+//     }   
+
+var color = document.getElementById("colorBtn");
+var r = document.querySelector(':root')
+
+color.addEventListener('click',() => {
+  r.style.setProperty('--blue', 'purple')
+} )
